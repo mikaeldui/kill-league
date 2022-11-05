be152a539133ea83a96af83902a717993d72330b
Mikael Dúi Bolinder <github@bolinder.uk>
2022-11-05 01:05:12 +0100

    Proper indentation in comments

--- Bat/KillLeague.bat
+++ Bat/KillLeague.bat
@@ -1,7 +1,7 @@
 :: taskkill = This tool is used to terminate tasks by process id (PID) or image name.
-:: /f = Specifies to forcefully terminate the process(es).
-:: /t = Terminates the specified process and any child processes which were started by it.
-:: /im = Specifies the image name of the process to be terminated. Wildcard '*' can be used to specify all tasks or image names.
+:: /f       = Specifies to forcefully terminate the process(es).
+:: /t       = Terminates the specified process and any child processes which were started by it.
+:: /im      = Specifies the image name of the process to be terminated. Wildcard '*' can be used to specify all tasks or image names.
 
 taskkill /f /t /im RiotClientUx.exe
 taskkill /f /t /im LeagueClient.exe
